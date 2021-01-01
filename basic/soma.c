f858d712eed0d7f57d0539f3c36ce8601e364c3f
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2020-12-31 23:43:58 -0300

    Add files via upload

---
+++ basic/soma.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int somar(int n1, int n2){
+
+    int soma = n1+ n2;
+    return soma;
+}
+
+
+int main(){
+    
+    int resultado = somar(10,20);
+
+    printf("%d");
+    return 0;
+}
