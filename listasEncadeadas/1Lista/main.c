52aed6cb549c7c7ba9431ac78d103fe9d8aa8890
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-01-26 23:39:34 -0300

    verificando se a lista está vazia antes e após inserção de dados

--- listasEncadeadas/1Lista/main.c
+++ listasEncadeadas/1Lista/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "lista.h"
@@ -15,6 +14,13 @@ int main()
     //cria lista vazia
     lista = criar_lista();
     
+    if(vazia(lista)){
+        printf("Lista vazia! \n");
+    }
+    else{
+        printf("Lista NÃO está vazia");
+    }
+    
     //inserindo uma série de elementos na lista
     lista = inserir_lista(lista,10);
     lista = inserir_lista(lista,30);
@@ -24,7 +30,13 @@ int main()
     lista = inserir_lista(lista,49);
     imprimir_lista(lista);
 
-
+ if(vazia(lista)){
+        printf("Lista vazia! \n");
+    }
+    else{
+        printf("Lista NÃO está vazia");
+    }
+    
     
     return 0;
 }
