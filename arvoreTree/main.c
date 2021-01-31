0d5c64169ede772b1d4e56403f5a3d8e17bdb79d
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-01-30 23:20:22 -0300

    Update main.c

--- arvoreTree/main.c
+++ arvoreTree/main.c
@@ -8,8 +8,12 @@ int main()
     Arv* a2 = criar('a', criar_vazia(), a1);
 
     Arv* a3 = criar('p', a1, a2);
+    
+    Arv* a4 = criar('p', a3, a2);
 
     printf("%d \n", vazia(a1));
-    imprimir(a3);
+    imprimir(a4);
+    
+    liberar(a4);
     return 0;
 }
