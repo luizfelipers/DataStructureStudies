7c21033a3bc0ad4df7fec0c6fcdd420abb494d97
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-01-30 23:52:14 -0300

    conferindo itens da lista, dentro da função main

--- arvoreTree/main.c
+++ arvoreTree/main.c
@@ -6,14 +6,15 @@ int main()
 {
     Arv* a1 = criar('m', criar_vazia(), criar_vazia());
     Arv* a2 = criar('a', criar_vazia(), a1);
-
+ 
     Arv* a3 = criar('p', a1, a2);
     
     Arv* a4 = criar('p', a3, a2);
 
     printf("%d \n", vazia(a1));
     imprimir(a4);
-    
+    printf("\n %d \n", pertence(a4, 'm'));
+    printf("%d \n", pertence(a4, 'z'));
     liberar(a4);
     return 0;
 }
