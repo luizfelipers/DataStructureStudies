d10c3e1c4b6e948051769b6452b00975bfe1050a
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-01-30 23:06:27 -0300

    imprime árvore

--- arvoreTree/main.c
+++ arvoreTree/main.c
@@ -7,5 +7,9 @@ int main()
     Arv* a1 = criar('m', criar_vazia(), criar_vazia());
     Arv* a2 = criar('a', criar_vazia(), a1);
 
+    Arv* a3 = criar('p', a1, a2);
+
+    printf("%d \n", vazia(a1));
+    imprimir(a3);
     return 0;
 }
