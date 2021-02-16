57051c14222b5ba9c1f8af5af8301ad32e32bdec
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-02-15 23:52:54 -0300

    functions implementations

--- grafos/graph.c
+++ grafos/graph.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 
 int main()
-{                           //a           b          c            d          e
+{         //hardcoding a matrix with it's values
+                            //a           b          c            d          e
     int matriz1[5][5] = {{0,0,1,0,0},{0,0,1,0,0},{1,1,0,1,1},{0,0,1,0,1},{0,0,1,1,0}};
     
+    // function that checks if there's a lik between nodes 
     int tem_ligacao(int x, int y)
     {
         return matriz1[x][y];
     }
-
+    //checking it there's a link between A and C
     if(tem_ligacao(0,2)==0){
         printf("não tem ligação de A para C");
     }
